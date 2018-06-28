6a6d48f7d70fd0faef010a9a1ef7a3a82e6a1fd2
Cirlan Andrei <andreynho2006@gmail.com>
2018-06-28 09:34:24 +0000

    make 3 routes to test the app

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Our Express app will go here!");
+var express = require("express");
+
+var app = express();
+
+// "/"=> "Hy there!"
+app.get("/", function(req, res) {
+    res.send("Hy there!");
+});
+// "/bye" => "goodbye"
+app.get("/bye", function(req, res) {
+    res.send("goodbye");
+})
+// "/dog" => "Moaw"
+app.get("/dog", function(req, res) {
+    console.log("Someone made a request to /dog");
+    res.send("Moaw!");
+})
+
+// Tell Express to listen for request (start server)
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("server has started!");
+});
